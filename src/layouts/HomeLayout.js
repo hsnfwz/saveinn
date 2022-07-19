e644ae9c011ea1537e0579720b08ac82282cf176
jag18 <juan_antonio_gonzalez@sfu.ca>
2022-07-18 21:50:45 -0700

    layout for landing page

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,12 +1,38 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, InputGroup, Navbar, Nav, NavItem } from "react-bootstrap";
 
 function HomeLayout() {
 
   return (
     <Container fluid>
+      <Navbar>
+        <Container fluid>
+          <Navbar.Brand>Save Inn</Navbar.Brand>
+          <Nav>
+            <Nav.Link>Product</Nav.Link>
+            <Nav.Link>About Us</Nav.Link>
+          </Nav>
+          <Button>Sign Up</Button>
+        </Container>
+      </Navbar>
       <Row>
         <Col>
-          Home
+          <Row>
+            <h2>Save more than what you spend. Welcome to the Save Inn!</h2>
+          </Row>
+          <Row>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique ornare gravida. Vivamus consequat nec lacus vel laoreet. </p>
+          </Row>
+          <Row>
+            <InputGroup>
+              <Form.Control type="text" placeholder="Email address"/>
+              <Button>
+                Sign up for Save Inn
+              </Button>
+            </InputGroup>
+          </Row>
+        </Col>
+        <Col>
+          Image
         </Col>
       </Row>
     </Container>
