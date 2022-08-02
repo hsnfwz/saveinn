3e3e27175f8b0c25d03383258d721bdd8feee7f5
Hussein Fawaz <husseinf@onswitchboard.com>
2022-08-01 21:34:01 -0700

    updated pg url

--- pg.js
+++ pg.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = 'postgres://postgres:postgres@localhost:5432/test';
+const connectionString = 'postgres://postgres:postgres@localhost:5432/saveinn';
 const pool = new Pool({ connectionString });
 
 module.exports = pool;
